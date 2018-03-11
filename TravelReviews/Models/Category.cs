c011248ba6aa4aefc84967417e929b72a438f333
Weiskri <kkweis13@gmail.com>
2018-03-11 17:00:58 -0400

    first commit

---
+++ TravelReviews/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviews.Models
+{
+    public class Category
+    {
+        // properties
+        [Key]
+
+        public int ID { get; set; }
+
+        [Display(Name="Category")]
+        public string Name { get; set; }
+
+        // navigation property
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReviews/Models/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviews.Models
+{
+    public class Review
+    {
+        // properties
+        [Key]
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Comments")]
+        public string Content { get; set; }
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime PublishedDate { get; set; }
+        [Display(Name = "Would Recommend")]
+        public bool Recommended { get; set; }
+
+        // foreign keys and navigation properties
+        [ForeignKey("Category"), Display(Name = "Category")] // changing foreignkey display to "Category" instead of CategoryID
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
